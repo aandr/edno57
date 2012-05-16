444000e4357e3c304b5f7adf7ba96bc89213dbdc
Kiril Vladimirov <kiril@vladimiroff.org>
2012-05-17 01:09:54 +0300

    Make urls.py know about class based views

--- edno57/urls.py
+++ edno57/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+
+from haikus.views import Homepage, UserPage
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^calculator$', 'calculator.views.calculator'),
-    url(r'^$', 'haikus.views.homepage', name = 'homepage'),
+    url(r'^$', Homepage.as_view(), name = 'homepage'),
     url(r'^add$', 'haikus.views.add_haiku', name = 'add-haiku'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name = 'login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name = 'logout'),
     url(r'^feedback$', 'feedback.views.contact_form', name = 'contact-form'),
-    url(r'^(?P<username>[a-zA-z0-9]+)$', 'haikus.views.user_page', name = 'user-page'),
+    url(r'^(?P<username>[a-zA-z0-9]+)$', UserPage.as_view(), name = 'user-page'),
 )
 
 if settings.DEBUG:
