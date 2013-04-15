0e58712da03e19e1f795613d9e77668c06885597
Alek Andreev <alek@zvuk.net>
2013-04-15 20:45:59 +0300

    change res to result in tests, thanks to Ilian

--- edno57/haikus/tests.py
+++ edno57/haikus/tests.py
@@ -14,17 +14,17 @@ class HaikuTest(unittest.TestCase):
         self.client.login(username='vanko', password='parola')
 
     def test_login(self):
-        res = self.client.get('/')
-        self.assertEqual(self.user, res.context['user'])
+        result = self.client.get('/')
+        self.assertEqual(self.user, result.context['user'])
 
     def test_add_haiku(self):
         haiku_text = "На клон изсъхнал настръхнали са птици след силна буря."
-        res = self.client.post('', {'text': haiku_text})
-        self.assertEqual(200, res.status_code)
-        res = self.client.get('/')
-        self.assertTrue(haiku_text.encode('UTF-8') in res.content)
+        result = self.client.post('', {'text': haiku_text})
+        self.assertEqual(200, result.status_code)
+        result = self.client.get('/')
+        self.assertTrue(haiku_text.encode('UTF-8') in result.content)
 
-    def test_loggedout_add_haiku(sefl):
+    def test_loggedout_add_haiku(kiril):
         c = Client()
 
         count_before = Haiku.objects.all().count()
@@ -32,4 +32,4 @@ class HaikuTest(unittest.TestCase):
         c.post('', {'text': haiku_text})
         count_after = Haiku.objects.all().count()
 
-        sefl.assertEqual(count_before, count_after)
+        kiril.assertEqual(count_before, count_after)
